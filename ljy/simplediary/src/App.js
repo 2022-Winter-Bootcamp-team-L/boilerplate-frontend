f507dbea1cfabc2bd5627b69dcae32905688954a
lee juyoung <88328887+phnml1@users.noreply.github.com>
2023-01-05 03:31:34 +0900

    feature1/dropdowninputandimage보여주기

---
+++ ljy/simplediary/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import DiaryEditor from './DiaryEditor';
+import { useState } from 'react';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <DiaryEditor/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ljy/tailwindprac/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.scss';
+import DragDrop from './DragDrop';
+import { useCallback } from 'react';
+
+function App() {
+  
+  
+  return (
+    <>
+    <DragDrop/>
+    </>
+  );
+}
+export default App;
